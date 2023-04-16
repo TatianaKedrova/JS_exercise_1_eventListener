963b2427d0ae75b6c071b18c66f69709012e58fb
TatianaKedrova <127103022+TatianaKedrova@users.noreply.github.com>
2023-04-16 09:30:27 +0200

    Function in Javascript that toggles the background color

---
+++ main.js
@@ -0,0 +1,25 @@
+
+let button = document.getElementById("red")
+button.addEventListener("click", function() {
+    let recuadro = document.getElementById("recuadro");
+    recuadro.style.backgroundColor = "red";
+});
+
+let b2 = document.getElementById("blue")
+b2.addEventListener("click", function() {
+    let recuadro = document.getElementById("recuadro");
+    recuadro.style.backgroundColor = "blue";
+});
+
+let b3 = document.getElementById("green")
+b3.addEventListener("click", function() {
+    let recuadro = document.getElementById("recuadro");
+    recuadro.style.backgroundColor = "green";
+});
+
+
+let b4 = document.getElementById("yellow")
+b4.addEventListener("click", function() {
+    let recuadro = document.getElementById("recuadro");
+    recuadro.style.backgroundColor = "yellow";
+});
